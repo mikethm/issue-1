298bc1a759e57be721edd6d2b2212bdfe4874124
Michael Tsang <mtsang@handymetrics.com>
2017-08-23 13:19:42 -0400

    updated to ionic 3.6.0 and angular-formio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { FormioModule } from 'ng2-formio';
+import { FormioModule } from 'angular-formio';
 
 @NgModule({
   declarations: [
